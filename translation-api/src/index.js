c2e97d8e2760e935b4b76b749eabc0765754f5f8
Gustavo Dechotti <75210025+GusDechotti@users.noreply.github.com>
2025-06-17 19:24:35 -0300

    Create Swagger

--- translation-api/src/index.js
+++ translation-api/src/index.js
@@ -1,15 +1,19 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
 const { connectDB } = require('./database');
 const { initBroker } = require('./messageBroker');
 const routes = require('./routes');
+const swaggerDocument = require('./swagger.json'); 
 
 const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Translation API is running!');
+  res.send('Translation API is running! Go to /api-docs for documentation.');
 });
 app.use(routes);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const PORT = 3000;
 
@@ -18,6 +22,7 @@ const startServer = async () => {
   await initBroker();
   app.listen(PORT, () => {
     console.log(`API Server is running on http://localhost:${PORT}`);
+    console.log(`Swagger UI available at http://localhost:${PORT}/api-docs`);
   });
 };
 
